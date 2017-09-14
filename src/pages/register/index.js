ec261ad4ffb580a07e06a492efcb08f7030793b7
ENN <ENN@Local.posh>
2017-09-15 01:11:49 +0900

    Register radio inputs

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,11 +1,37 @@
 import EmailValidator from 'email-validator';
+import leftPad from 'left-pad';
 
 class RegisterCtrl {
     static get $inject() {
-        return ['$state', 'LoginSvc']
+        return ['$scope', '$state', 'LoginSvc']
     }
 
-    // =============== 인라인 CSS
+    // =============== Property
+    get BirthMonth() {
+        return this.registerObj.birth.month;
+    }
+
+    set BirthMonth(val) {
+        this.registerObj.birth.month = val;
+    }
+
+    get BirthDay() {
+        return this.registerObj.birth.day
+    }
+
+    set BirthDay(val) {
+        this.registerObj.birth.day = val;
+    }
+
+    get BirthYear() {
+        return this.registerObj.birth.year;
+    }
+
+    set BirthYear(val) {
+        this.registerObj.birth.year = val;
+    }
+
+    // =============== CSS
     /**
      * 선택한 프로필 이미지의 인라인 백그라운드 이미지 스타일
      * @returns {*}
@@ -14,17 +40,20 @@ class RegisterCtrl {
         return this.imagePreview === null ?
             null : {'background-image': `url(${this.imagePreview})`};
     }
+
     get SignUpLabel() {
         return this.pending ?
             '...' : 'SIGN UP';
     }
+
     get EmailStyle() {
         if (this.registerObj.email === null) {
             return null;
         }
-        return ! EmailValidator.validate(this.registerObj.email) ?
+        return !EmailValidator.validate(this.registerObj.email) ?
             this.invalidStyle : null;
     }
+
     get PasswordStyle() {
         if (this.registerObj.pwd === null) {
             return null;
@@ -35,19 +64,105 @@ class RegisterCtrl {
         return this.registerObj.pwd.length < 6 ?
             this.invalidStyle : null;
     }
+
     get PasswordConfirmStyle() {
         return this.registerObj.pwd !== this.registerObj.pwdConfirm ?
             this.invalidStyle : null;
     }
 
-    constructor($state, LoginSvc) {
+    genderChecked(val) {
+        return this.registerObj.gender === val ?
+            'is-checked' : null;
+    }
+
+    nationChecked(val) {
+        return this.registerObj.nation === val ?
+            'is-checked' : null;
+    }
+
+    sanitizeBirth() {
+        this.sanitizeMonth();
+        this.sanitizeDay();
+        this.sanitizeYear();
+    }
+    sanitizeMonth() {
+        if (this.BirthMonth === null) {
+            return;
+        }
+        let val = parseInt(this.BirthMonth);
+        if (Number.isNaN(val) || val < 1) {
+            this.BirthMonth = 1;
+        }
+        else if (val > 12) {
+            this.BirthMonth = 12;
+        }
+    }
+    sanitizeDay() {
+        if (this.BirthDay === null) {
+            return;
+        }
+        let val = parseInt(this.BirthDay);
+        if (Number.isNaN(val) || val < 1) {
+            this.BirthDay = 1;
+        }
+        else {
+            switch (this.BirthMonth) {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    if (val > 31) {
+                        this.BirthDay = 31;
+                    }
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    if (val > 30) {
+                        this.BirthDay = 30;
+                    }
+                    break;
+                case 2:
+                    if (this.BirthYear !== null
+                        && ((this.BirthYear % 4 === 0) && (this.BirthYear % 100 !== 0))
+                        || (this.BirthYear % 400 === 0)) {
+                        if (this.BirthDay > 29) {
+                            this.BirthDay = 29;
+                        }
+                    }
+                    else if (this.BirthYear > 28) {
+                        this.BirthDay = 28;
+                    }
+                    break;
+            }
+        }
+    }
+    sanitizeYear() {
+        if (this.BirthYear === null) {
+            return;
+        }
+        let val = parseInt(this.BirthYear);
+        if (Number.isNaN(val) || val < 1900) {
+            this.BirthYear = 1990;
+        }
+        else if (val > 2020) {
+            this.BirthYear = 2020;
+        }
+    }
+
+    constructor($scope, $state, LoginSvc) {
+        this.$scope = $scope;
         this.$state = $state;
         this.LoginSvc = LoginSvc;
 
         this.imagePreview = null;
         this.imageData = null;
         this.form = document.getElementById('form_register');
-        this.invalidStyle = {'border-color': 'rgba(255, 55, 55, .25)'};
+        this.invalidStyle = {'box-shadow': '0 0 5.9px 3.2px rgba(255, 0, 0, 0.22)'};
 
         /**
          * 회원가입 요청 전송 여부
@@ -57,24 +172,23 @@ class RegisterCtrl {
         this.pending = false;
         /**
          * 회원가입 오브젝트 원본
-         * @type {{
-         * login: string,
-         * name: string,
-         * email: null,
-         * pwd: string,
-         * pwdConfirm: string
-         * }}
          */
         this.registerTemplate = {
             login: null,
             name: null,
             email: null,
             pwd: null,
-            pwdConfirm: null
+            pwdConfirm: null,
+            birth: {
+                month: null,
+                day: null,
+                year: null
+            },
+            gender: null,
+            nation: null
         };
         /**
          * 회원가입 오브젝트
-         * @type registerTemplate
          */
         this.registerObj = angular.copy(this.registerTemplate);
     }
@@ -84,7 +198,7 @@ class RegisterCtrl {
             return;
         }
         // 이메일 검사
-        if (! EmailValidator.validate(this.registerObj.login)) {
+        if (!EmailValidator.validate(this.registerObj.login)) {
             alert('이메일이 아니에요!');
             return;
         }
