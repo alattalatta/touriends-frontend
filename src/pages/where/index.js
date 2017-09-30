74efaf54e360dc5dadf55d20e2065c231b9aea0b
Kim-Gwang-Un <kgo0748@gmail.com>
2017-09-30 15:32:43 +0900

    터치 시 값 넘겨주는거전까지 다함

--- src/pages/where/index.js
+++ src/pages/where/index.js
@@ -1,5 +1,19 @@
 class WhereCtrl {
+    constructor() {
+        this.locations = ['Eunpyeong', 'Gangbuk', 'Dobong', 'Gangnam'
+        , 'Jongno' , 'Seongbuk', 'Dongdaemun', 'Mapo',
+        'Gangseo', 'Jung-gu', 'Seong-dong', 'Nowon',
+        'Yeongdeungpo','Yangcheon','Dongjak','Seocho',
+        'Songpa','Gangdong','Guro','Geumcheon',
+        'Gwanak','Gwangjin','Jungnang','Yongsan','Seodaemun'];
+        this.selected = null;
 
+        this.whereSelect = function ($index){
+                this.selected = $index;
+        }
+    }
 }
+
+
 
 export default angular.module('touriends.page.where', ['touriends']).controller('WhereCtrl', WhereCtrl).name;
