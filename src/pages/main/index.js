76762f53b8135f14df6f323e667816d129e5f177
urty5656 <urty5656@gmail.com>
2017-10-01 13:12:20 +0900

    Main to when, when to where

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,18 +1,16 @@
-import param from 'jquery-param';
+function MainCtrl($state) {
+	this.nav = ['matching', 'community', 'home', 'attraction', 'mypage'];
 
-function MainCtrl() {
-  this.nav = ['matching','community','home','attraction','mypage'];
-  this.start = function(){
-    alert('start~');
-  }
-  this.navbar = function(data){
-    for(var i=0; i<this.nav.length; i++){
-      if(data===this.nav[i])
-        alert(data);
-    }
-  }
+	this.start = function () {
+		$state.go('when');
+	};
+	this.navbar = function (data) {
+		for (var i = 0; i < this.nav.length; i++) {
+			if (data === this.nav[i])
+				alert(data);
+		}
+	}
 }
 
-MainCtrl.$inject = [];
-
+MainCtrl.$inject = ['$state'];
 export default angular.module('touriends.page.main', ['touriends']).controller('MainCtrl', MainCtrl).name;
--- src/pages/when/index.js
+++ src/pages/when/index.js
@@ -1,5 +1,14 @@
 class WhenCtrl {
+	static get $inject() {
+		return ['$state'];
+	}
 
+	constructor($state) {
+		this.$state = $state;
+	}
+	goNext() {
+		this.$state.go('where');
+	}
 }
 
 export default angular.module('touriends.page.when', ['touriends']).controller('WhenCtrl', WhenCtrl).name;
