f0ec5d3e4b1c28340a87cb7bbf6b475c228115d3
Kim-Gwang-Un <kgo0748@gmail.com>
2017-10-30 18:24:46 +0900

    po파일 변경 및 attraction파트 한글변환 완료

--- src/app.js
+++ src/app.js
@@ -23,6 +23,7 @@ let app = angular
 	.run(['gettextCatalog', (gettextCatalog) => {
 		gettextCatalog.debug = true;
 		gettextCatalog.loadRemote(`${locale_url}/ko.json`);
+		console.log(gettextCatalog);
 	}]);
 
 (require('./config').default)(app);
